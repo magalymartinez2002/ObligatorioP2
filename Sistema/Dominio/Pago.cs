0b77fa8a2c3a4d3e01da6b8b331dfd2bc4bf7af3
Magaly Martinez <martinezmagaly0@gmail.com>
2025-09-10 20:40:43 -0300

    agrego de clases

---
+++ Sistema/Dominio/Pago.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pago
+    {
+
+        private int _id;
+        private string _descripcion;
+        private MetodoDePago _metodoDePago;
+        private TipoDeGasto _tipoDeGasto;
+        private Usuario _usuario;
+
+
+        public Pago( string descripcion, MetodoDePago metodoDePago, TipoDeGasto tipoDeGasto, Usuario usuario)
+        {
+            
+            _descripcion = descripcion;
+            _metodoDePago = metodoDePago;
+            _tipoDeGasto = tipoDeGasto;
+            _usuario = usuario;
+        }
+    }
+}
---
+++ Sistema/Dominio/TipoDeGasto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TipoDeGasto
+    {
+        private string _nombre;
+            private string _descripcion;
+    }
+}
