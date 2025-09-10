a94e14266e53b09dfaec10e1563db97e80273f84
Magaly Martinez <martinezmagaly0@gmail.com>
2025-09-10 18:34:08 -0300

    Dominio

---
+++ Sistema/Dominio/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+
+        private string _noombre;
+        private string _apellido;
+        private string _email;
+        private Equipo _equipo;
+        private DateTime _fechaIngreso;
+    }
+}
--- Sistema/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Sistema/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319155c3ad3cede552d9d5d14edbdb6c0cdc421b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Cliente")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
